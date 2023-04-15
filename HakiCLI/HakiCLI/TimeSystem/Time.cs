6b3d3888b8eca817fe5d2275cd433169196b8873
Vito Scaletta <lyaguxmaslo@gmail.com>
2023-04-15 15:06:07 +0300

    Implemented simple time system

---
+++ HakiCLI/HakiCLI/TimeSystem/Time.cs
@@ -0,0 +1,38 @@
+﻿namespace Runtime
+{
+    public static class Time
+    {
+        public const int fixedUpdateRateInMilliseconds = 100;
+        public const int updateRateInMilliseconds = 10;
+
+        public static event Action FixedUpdate;
+
+        private static int _lastFixedUpdateTime = 0;
+
+        public static float SecondsUntilStart { get; private set; }
+
+        static Time() => Task.Run(StartUpdateRate);
+
+        private static void StartUpdateRate()
+        {
+            while (true)
+            {
+                Thread.Sleep(updateRateInMilliseconds);
+
+                UpdateValues();
+            }
+        }
+
+        private static void UpdateValues()
+        {
+            SecondsUntilStart += updateRateInMilliseconds / 1000f;
+            _lastFixedUpdateTime += updateRateInMilliseconds;
+
+            if (_lastFixedUpdateTime < fixedUpdateRateInMilliseconds)
+            {
+                FixedUpdate?.Invoke();
+                _lastFixedUpdateTime = 0;
+            }
+        }
+    }
+}
